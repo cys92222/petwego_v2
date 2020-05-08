5af08ad3e1d33e4a76690c56931d463d76f5ca20
chlqhdgus1 <chlqhdgus1@naver.com>
2020-05-08 11:19:01 +0900

    SNS Vo 봉현

---
+++ src/main/java/com/example/demo/vo/Pic_BoardVo.java
@@ -0,0 +1,18 @@
+package com.example.demo.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Pic_BoardVo {
+	private int photo_no;
+	private Date photo_date;
+	private String photo_detail;
+	private String user_id;
+	
+}
---
+++ src/main/java/com/example/demo/vo/Pic_Board_FileVo.java
@@ -0,0 +1,15 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pic_Board_FileVo {
+
+	private int photo_file_no;
+	private String photo_file_name;
+	private int photo_no;
+}
