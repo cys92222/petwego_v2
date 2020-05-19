4442b5d9e25f26d672c4fbd294b0e15bc0eca311
sngsw111 <lww891110@naver.com>
2020-05-19 17:00:49 +0900

    원우) 05/19 ReplyVo

--- src/main/java/com/example/demo/vo/ReplyVo.java
+++ src/main/java/com/example/demo/vo/ReplyVo.java
@@ -2,53 +2,20 @@ package com.example.demo.vo;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ReplyVo {
 	private int t_num;
 	private int t_r_num;
 	private String t_r_content;
 	private String user_id;
 	private Date regdate;
-	
-	public int getT_num() {
-		return t_num;
-	}
-	
-	public void setT_num(int t_num) {
-		this.t_num = t_num;
-	}
-	
-	public int getT_r_num() {
-		return t_r_num;
-	}
-	
-	public void setT_r_num(int t_r_num) {
-		this.t_r_num = t_r_num;
-	}
-	
-	public String getT_r_content() {
-		return t_r_content;
-	}
-	public void setT_r_content(String t_r_content) {
-		this.t_r_content = t_r_content;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public Date getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(Date regdate) {
-		this.regdate = regdate;
-	}
-	
-	@Override
-	public String toString() {
-		return "ReplyVo [t_num=" + t_num + ", t_r_num=" + t_r_num + ", t_r_content=" + t_r_content + ", user_id=" + user_id + ", regdate="
-				+ regdate + "]";
-	}
-	
+	private String secret_reply;
 	
 }
