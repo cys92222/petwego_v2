3077055a2159a21673d95a16409d46acb8a3878d
cys92222 <cys92222@naver.com>
2020-05-19 20:46:22 +0900

    영수) 5월19일 / aop관련 파일 주석

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.util.AopLog.NoLogging;
+//import com.example.demo.util.AopLog.NoLogging;
 
 //민아) 5/19, 메인페이지 @NoLogging 처리 
 @RestController
 public class MainController {
 	
 	//사이트 메인페이지
-	@NoLogging
+//	@NoLogging
 	@RequestMapping("/MainPage")
 	public static ModelAndView mainPage() {
 		ModelAndView mav = new ModelAndView();
