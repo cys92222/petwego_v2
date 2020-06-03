b3d35b54b1d7b37f13d56ee07ffe11e842a2833a
susu04-kor <suin0304_sw@google.com>
2020-06-03 11:23:27 +0900

    수인) 6월3일 / 사라진 프로필 사진 다시 추가

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,16 +1,12 @@
 package com.example.demo.controller;
 
 
-import java.io.Console;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
